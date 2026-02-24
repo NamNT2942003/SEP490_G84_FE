0c18aabf67aa83450755833e88b2cb155bcf9b02
dat092k3 <datnthe171830@fpt.edu.vn>
2026-02-24 19:08:16 +0700

    ui

--- SEP490_G84_FE/src/routes/AppRouter.jsx
+++ SEP490_G84_FE/src/routes/AppRouter.jsx
@@ -4,7 +4,8 @@ import ForgotPassword from '@/features/auth/screens/ForgotPassword';
 import MainLayout from '@/components/layout/MainLayout'; // Import Layout
 import Dashboard from '@/features/dashboard/screens/Dashboard'; 
 import ResetPassword from '@/features/auth/screens/ResetPassword';
-
+import BookingSummary from '@/features/booking/components/BookingSummary';
+import GuestInformation from '@/features/booking/screens/GuestInformation';
 // Component giả cho các trang chưa làm
 const RoomList = () => <h1>Danh sách phòng (Đang phát triển)</h1>;
 
@@ -16,7 +17,9 @@ const AppRouter = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/reset-password" element={<ResetPassword />} />
-      
+      <Route path="/BookingSummary" element={<BookingSummary />}/>
+      <Route path="/GuestInformation" element={<GuestInformation />} />
+
       {/* --- NHÓM 2: CÁC TRANG PRIVATE (CÓ FULL LAYOUT) --- */}
       {/* Cách dùng: Bọc Component con vào trong MainLayout */}
       
